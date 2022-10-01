fdaccadd58c72b1d9ad400421d9b6954362aff2f
Joose Lohi <joose.lohi01@gmail.com>
2022-10-01 14:48:49 +0300

    Convert HomeTab to StatefulWidget

--- app/lib/home_tab.dart
+++ app/lib/home_tab.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   static const title = "Home";
   static const iosIcon = Icon(CupertinoIcons.home);
 
   const HomeTab({super.key});
 
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
   Widget _buildBody(BuildContext context) {
     return Column();
   }
